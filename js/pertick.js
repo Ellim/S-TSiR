edc567b0da59df69148ab8ed5b771304a813009d
Ellim <seiferttyrindal@gmail.com>
2016-01-05 11:43:16 -0700

    Added updatefood function

--- js/pertick.js
+++ js/pertick.js
@@ -1,9 +1,12 @@
 
 var waterinterval;
+var foodinterval;
 var watertick = 2000;
+var foodtick = 500;
 var water = 0;
 var waterrate = 20;
 var watermax = 1000;
+var foodrate = 0;
 
 
 function updatewater() {
@@ -23,7 +26,22 @@ function updatewater() {
 	}, watertick);
 }
 	
-
+function updatefood() {
+  clearInterval(foodinterval);
+  foodinterval = setInterval(function() {
+	if (food < foodmax) {
+		food += foodrate;
+	}
+	if (food > foodmax) {
+		food = foodmax;
+	}
+	var tempvar = document.getElementById("currentfood");
+	tempvar.innerHTML = currentfood;
+	var tempvar2 = document.getElementById("foodps");
+	tempvar2.innerHTML = '+' + (foodrate/(foodtick/1000)).toFixed(2) + '/sec';
+	document.getElementById("foodbar").style.width = ((currentfood/foodmax)* 100) + '%';
+	}, foodtick);
+}
 
 
 document.getElementById("newgame").onclick = function() {
