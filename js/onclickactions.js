95a58c1d80b1ef9018814534eb70512c290d9234
Ellim <seiferttyrindal@gmail.com>
2016-01-05 10:08:24 -0700

    Update onclickactions.js

--- js/onclickactions.js
+++ js/onclickactions.js
@@ -1,3 +1,7 @@
+var currentfood = 150;
+var foodmax = 1500;
+var totalexp = 0;
+
 document.getElementById("Forage").onclick = function() {
   forage(1);   
 };
@@ -6,6 +10,7 @@ function forage(m) {
 	currentfood += m;
 	totalexp += m ;
 	if (currentfood > foodmax) {currentfood = foodmax};
-  var tempvar = document.getElementById("currentfood");
-  tempvar.innerHTML = currentfood.toFixed(2);
+  	var tempvar = document.getElementById("currentfood");
+  	tempvar.innerHTML = currentfood.toFixed(2);
+  	$('#foodbar').width(((currentfood/foodmax)* 100) + '%');
 }
