f7c18ae40cf5c6d6042f4ac5874464d7407ac4ad
Ellim <seiferttyrindal@gmail.com>
2016-02-04 11:22:19 -0700

    Update onclickactions.js

--- js/onclickactions.js
+++ js/onclickactions.js
@@ -220,7 +220,7 @@ document.getElementById("hutbutton").onclick = function() {
 };
 
 document.getElementById("unlockminebutton").onclick = function() {
-	if (talentpoints >= 5) {
+	if (talentpoints >= 0) {
 		talentpoints -= 5;
 		$('#minebuttondiv').removeClass('hidden');
 		$('#unlockminebutton').addClass('hidden');
