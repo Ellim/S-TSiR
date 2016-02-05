eead56d734ae0997b572a9bbcc5e31e86ea40bb6
Ellim <seiferttyrindal@gmail.com>
2016-02-05 12:48:43 -0700

    Update pertick.js

--- js/pertick.js
+++ js/pertick.js
@@ -1,3 +1,47 @@
+function updateworkers() {
+	var tempvar = document.getElementById("freeworkers");
+	tempvar.innerHTML = freeworkers.toFixed(0);
+	var tempvar2 = document.getElementById("researchernum");
+	tempvar2.innerHTML = researchers.toFixed(0);
+	var tempvar3 = document.getElementById("farmernum");
+	tempvar3.innerHTML = farmers.toFixed(0);
+	var tempvar4 = document.getElementById("loggernum");
+	tempvar4.innerHTML = loggers.toFixed(0);
+	var tempvar5 = document.getElementById("minernum");
+	tempvar5.innerHTML = miners.toFixed(0);
+	var tempvar6 = document.getElementById("warriornum");
+	tempvar6.innerHTML = warriors.toFixed(0);	
+}
+
+function updateresources() {
+	document.getElementById("foodbar").style.width = ((currentfood/foodmax)* 100) + '%';
+	document.getElementById("waterbar").style.width = ((water/watermax) * 100) + '%';
+	document.getElementById("expbar").style.width = ((totalexp/exptnl)* 100) + '%';
+	var tempvar4 = document.getElementById("total_RP");
+	tempvar4.innerHTML = researchpoints.toFixed(0);
+	var tempvar5 = document.getElementById("pop");
+	tempvar5.innerHTML = population.toFixed(0);
+	var tempvar6 = document.getElementById("popmax");
+	tempvar6.innerHTML = popmax.toFixed(0);
+	var tempvar7 = document.getElementById("total_wood");
+	tempvar7.innerHTML = wood.toFixed(2);
+	var tempvar8 = document.getElementById("woodmax");
+	tempvar8.innerHTML = woodmax.toFixed(0);
+	var tempvar9 = document.getElementById("total_stone");
+	tempvar9.innerHTML = stone.toFixed(2);
+	var tempvar10 = document.getElementById("stonemax");
+	tempvar10.innerHTML = stonemax.toFixed(0);
+	var tempvar11 = document.getElementById("total_coal");
+	tempvar11.innerHTML = coal.toFixed(2);
+	var tempvar12 = document.getElementById("coalmax");
+	tempvar12.innerHTML = coalmax.toFixed(0);
+	var tempvar13 = document.getElementById("total_clay");
+	tempvar13.innerHTML = clay.toFixed(2);
+	var tempvar14 = document.getElementById("claymax");
+	tempvar14.innerHTML = claymax.toFixed(0);
+	var tempvar15 = document.getElementById("total_mana");
+	tempvar15.innerHTML = mana.toFixed(0);
+}
 
 function autosave() {
 	clearInterval(saveinterval);
@@ -173,7 +217,8 @@ document.getElementById("newgame").onclick = function() {
 		updateminer();
 		updateresearcher();
 		autosave();
-		
+		updateresources();
+		updateworkers();
 }
 
 document.getElementById("loadgame").onclick = function() {
@@ -185,51 +230,25 @@ document.getElementById("loadgame").onclick = function() {
 		updateminer();
 		updateresearcher();
 		autosave();
+		updateresources();
+		updateworkers();
 		
 		if (burntalent > 0) {
-			$('#Burn').removeClass('hidden');
-			$('#burn10').removeClass('hidden');
-			$('#burn25').removeClass('hidden');
-			$('#burn100').removeClass('hidden'); 
-			$('#burncheckboxes').removeClass('hidden'); 
-			$('#unlockburnbutton').addClass('hidden');
-			$('#burntalent').addClass('hidden');
-		}
-		
-		if (minetalent > 0) {
-			$('#Mine').removeClass('hidden');
-			$('#mine10').removeClass('hidden');
-			$('#mine25').removeClass('hidden');
-			$('#mine100').removeClass('hidden');
-			$('#unlockminebutton').addClass('hidden');
+			$('#minebuttondiv').removeClass('hidden');
 			$('#minetalent').addClass('hidden');
 		}
 		
-		document.getElementById("waterbar").style.width = ((water/watermax) * 100) + '%';
-		document.getElementById("foodbar").style.width = ((currentfood/foodmax)* 100) + '%';
-		document.getElementById("expbar").style.width = ((totalexp/exptnl)* 100) + '%';
-		
-		var tempvar = document.getElementById("fruittreescost");
-		tempvar.innerHTML = fruittreescost.toFixed(2);
-		var tempvar2 = document.getElementById("maxfood");
-		tempvar2.innerHTML = foodmax.toFixed(2);
-		var tempvar3 = document.getElementById("fruittreenum");
-		tempvar3.innerHTML = fruittrees.toFixed(0);
-		var tempvar4 = document.getElementById("total_wood");
-		tempvar4.innerHTML = wood.toFixed(2);
-		var tempvar5 = document.getElementById("currentfood");
-		tempvar5.innerHTML = currentfood.toFixed(2);
-		
+		if (minetalent > 0) {
+			$('#burnbuttondiv').removeClass('hidden');
+			$('#burntalent').addClass('hidden');
+		}
+
 		var tempvar6 = document.getElementById("riverupgradecost");
 		tempvar6.innerHTML = riverupgradecost.toFixed(0);
 		var tempvar7 = document.getElementById("riverlevel");
 		tempvar7.innerHTML = riverlevel;
 		var tempvar8 = document.getElementById("riverproduction");
 		tempvar8.innerHTML = ((riverupgradecost * 20)/(watertick/1000)).toFixed(2);
-		var tempvar9 = document.getElementById("talentpoints");
-		tempvar9.innerHTML = talentpoints;
-		var tempvar10 = document.getElementById("waterps");
-		tempvar10.innerHTML = '+' + (waterrate/(watertick/1000)).toFixed(2) + '/sec';
 		
 		var tempvar11 = document.getElementById("hutnum");
 		tempvar11.innerHTML = huts;
@@ -237,21 +256,6 @@ document.getElementById("loadgame").onclick = function() {
 		tempvar12.innerHTML = hutcost.toFixed(2);
 		var tempvar13 = document.getElementById("hutexp");
 		tempvar13.innerHTML = hutcost.toFixed(2);
-		var tempvar14 = document.getElementById("popmax");
-		tempvar14.innerHTML = popmax.toFixed(0);
-		var tempvar15 = document.getElementById("currentwater");
-		tempvar15.innerHTML = water.toFixed(2);
-		
-		var tempvar16 = document.getElementById("exptotal");
-		tempvar16.innerHTML = totalexp.toFixed(0);
-		var tempvar17 = document.getElementById("exptnl");
-		tempvar17.innerHTML = exptnl.toFixed(0);
-		var tempvar18 = document.getElementById("current_level");
-		tempvar18.innerHTML = currentlevel.toFixed(0);
-		var tempvar19 = document.getElementById("pop");
-		tempvar19.innerHTML = population.toFixed(0);
-		var tempvar20 = document.getElementById("maxwater");
-		tempvar20.innerHTML = watermax.toFixed(2);
 		
 		var tempvar21 = document.getElementById("granarynum");
 		tempvar.innerHTML = granaries;
@@ -259,37 +263,12 @@ document.getElementById("loadgame").onclick = function() {
 		tempvar22.innerHTML = granarywoodcost;
 		var tempvar23 = document.getElementById("granarystonecost");
 		tempvar23.innerHTML = granarystonecost;
-		var tempvar24 = document.getElementById("woodmax");
-		tempvar24.innerHTML = woodmax.toFixed(2);
-		var tempvar25 = document.getElementById("stonemax");
-		tempvar25.innerHTML = stonemax.toFixed(2);
-		
-		var tempvar26 = document.getElementById("coalmax");
-		tempvar26.innerHTML = coalmax.toFixed(2);
-		var tempvar27 = document.getElementById("claymax");
-		tempvar27.innerHTML = claymax.toFixed(2);
+
 		var tempvar28 = document.getElementById("shnum");
 		tempvar28 = storehouses;
 		var tempvar29 = document.getElementById("shcost");
 		tempvar29.innerHTML = shcost.toFixed(2);
-		var tempvar30 = document.getElementById("total_stone");
-		tempvar30.innerHTML = stone.toFixed(2);
-		
-		var tempvar31 = document.getElementById("total_coal");
-		tempvar31.innerHTML = coal.toFixed(2);
-		var tempvar32 = document.getElementById("total_clay");
-		tempvar32.innerHTML = clay.toFixed(2);
-		var tempvar33 = document.getElementById("total_mana");
-		tempvar33.innerHTML = mana.toFixed(0);
-		var tempvar34 = document.getElementById("total_steam");
-		tempvar34.innerHTML = steam.toFixed(0);
-		var tempvar35 = document.getElementById("fruittreescost");
-		tempvar35.innerHTML = fruittreescost.toFixed(2);
-		
-		var tempvar36 = document.getElementById("farmernum");
-		tempvar36.innerHTML = farmers;
-		var tempvar37 = document.getElementById("loggernum");
-		tempvar37.innerHTML = loggers;
+
 }
 	
 /*
