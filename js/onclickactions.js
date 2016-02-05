ba44ce3f32547a4537a61ea717b40406dca7fd9e
Ellim <seiferttyrindal@gmail.com>
2016-02-05 12:43:35 -0700

    0.0.7 Update, move update functions to pertick

--- js/onclickactions.js
+++ js/onclickactions.js
@@ -361,56 +361,7 @@ function updatetotalexp() {
   document.getElementById("expbar").style.width = ((totalexp/exptnl)* 100) + '%';
 }
 
-function updateworkers() {
-	
-	var tempvar = document.getElementById("freeworkers");
-	tempvar.innerHTML = freeworkers.toFixed(0);
-	var tempvar2 = document.getElementById("researchernum");
-	tempvar2.innerHTML = researchers.toFixed(0);
-	var tempvar3 = document.getElementById("farmernum");
-	tempvar3.innerHTML = farmers.toFixed(0);
-	var tempvar4 = document.getElementById("loggernum");
-	tempvar4.innerHTML = loggers.toFixed(0);
-	var tempvar5 = document.getElementById("minernum");
-	tempvar5.innerHTML = miners.toFixed(0);
-	var tempvar6 = document.getElementById("warriornum");
-	tempvar6.innerHTML = warriors.toFixed(0);
-	var tempvar7 = document.getElementById("fruittreenum");
-	tempvar7 = fruittrees.toFixed(0);
-	
-	
-}
 
-function updateresources() {
-	document.getElementById("foodbar").style.width = ((currentfood/foodmax)* 100) + '%';
-	document.getElementById("waterbar").style.width = ((water/watermax) * 100) + '%';
-	document.getElementById("expbar").style.width = ((totalexp/exptnl)* 100) + '%';
-	var tempvar4 = document.getElementById("total_RP");
-	tempvar4.innerHTML = researchpoints.toFixed(0);
-	var tempvar5 = document.getElementById("pop");
-	tempvar5.innerHTML = population.toFixed(0);
-	var tempvar6 = document.getElementById("popmax");
-	tempvar6.innerHTML = popmax.toFixed(0);
-	var tempvar7 = document.getElementById("total_wood");
-	tempvar7.innerHTML = wood.toFixed(2);
-	var tempvar8 = document.getElementById("woodmax");
-	tempvar8.innerHTML = woodmax.toFixed(0);
-	var tempvar9 = document.getElementById("total_stone");
-	tempvar9.innerHTML = stone.toFixed(2);
-	var tempvar10 = document.getElementById("stonemax");
-	tempvar10.innerHTML = stonemax.toFixed(0);
-	var tempvar11 = document.getElementById("total_coal");
-	tempvar11.innerHTML = coal.toFixed(2);
-	var tempvar12 = document.getElementById("coalmax");
-	tempvar12.innerHTML = coalmax.toFixed(0);
-	var tempvar13 = document.getElementById("total_clay");
-	tempvar13.innerHTML = clay.toFixed(2);
-	var tempvar14 = document.getElementById("claymax");
-	tempvar14.innerHTML = claymax.toFixed(0);
-	var tempvar15 = document.getElementById("total_mana");
-	tempvar15.innerHTML = mana.toFixed(0);
-	
-}
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
