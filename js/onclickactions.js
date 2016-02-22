7bcb56783587caec8ffc5fadc1d32db88902ad0b
Ellim <seiferttyrindal@gmail.com>
2016-02-22 12:28:49 -0700

    Update onclickactions.js

--- js/onclickactions.js
+++ js/onclickactions.js
@@ -64,7 +64,6 @@ document.getElementById("tradetalent").onclick = function() {
 	if ($('#tradetalent').hasClass('btn-danger')) {
 		$('#tradetalent').removeClass('btn-danger');
 	} 
-	alert("test");
 }
 
 document.getElementById("consttalent").onclick = function() {
